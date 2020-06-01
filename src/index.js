4b246c55cb2b7714eb492dec2067280d3a8f12a9
Vincent0Lee <vincent0lee95@gmail.com>
2020-06-01 23:52:54 +0800

    Add default get response

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) =>{
+  res.send('Hello, This is a task manager api')
+})
+
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
 })
